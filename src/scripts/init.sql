01cb548325671f9a7e0fac41c05bb14eecda6e3f
Matheus Ferreira <matheus.silva@sou.inteli.edu.br>
2025-05-26 11:25:25 -0300

    fix: corrige fuso-horarios das datas do modelo fisico

--- src/scripts/init.sql
+++ src/scripts/init.sql
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS eventos (
   titulo VARCHAR(50) NOT NULL,
   descricao TEXT,
   local TEXT NOT NULL,
-  data_hora_inicio TIMESTAMP NOT NULL,
-  data_hora_fim TIMESTAMP NOT NULL,
+  data_hora_inicio TIMESTAMP WITH TIME ZONE NOT NULL,
+  data_hora_fim TIMESTAMP WITH TIME ZONE NOT NULL,
   limite_convidados INTEGER NOT NULL 
 );
 
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS organizadores_eventos (
 CREATE TABLE IF NOT EXISTS convidados_evento (
   id SERIAL PRIMARY KEY,
   confirmado BOOLEAN DEFAULT FALSE,
-  data_confirmacao TIMESTAMP,
+  data_confirmacao TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   id_evento INT,
   id_convidado INT,
   FOREIGN KEY (id_evento) REFERENCES eventos(id),
@@ -40,6 +40,6 @@ CREATE TABLE mensagens_evento (
   id_evento INT NOT NULL REFERENCES eventos(id) ON DELETE CASCADE,
   id_usuario INT REFERENCES usuarios(id) ON DELETE SET NULL,
   conteudo TEXT NOT NULL,
-  data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  data_envio TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 

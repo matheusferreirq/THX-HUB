5a89066915e53d42bf72c2e710071af8578f0bf1
Matheus Ferreira <matheus.silva@sou.inteli.edu.br>
2025-06-01 04:25:51 -0300

    feat: adiciona landing page

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const path = require('path');
 
 const userRoutes = require('./src/routes/userRoutes');
 const eventRoutes = require('./src/routes/eventRoutes');
 const organizerRoutes = require('./src/routes/organizerRoutes');
 const guestRoutes = require('./src/routes/guestRoutes');
 const messageRoutes = require('./src/routes/messageRoutes');
+const landingPageRoutes = require('./src/routes/landingPageRoutes');
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
+
+app.use('/', landingPageRoutes);
 app.use('/usuarios', userRoutes);
 app.use('/eventos', eventRoutes);
 app.use('/organizadores', organizerRoutes);
